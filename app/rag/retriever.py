fed1dbb5f5587d4907617149b1e4a1cefebafe0b
Francesca Poli <francescapoli1998@gmail.com>
2026-04-17 16:07:24 +0200

    test commit

--- app/rag/retriever.py
+++ app/rag/retriever.py
@@ -1,14 +1,21 @@
 from langchain_community.vectorstores.faiss import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader
+##data
+from data__temp import MEDICAL_DOCS
+from langchain.schema import Document
 
-# carica una volta sola
-loader = TextLoader("medical_data.txt")
-documents = loader.load()
+docs = [
+    Document(
+        page_content=f"{item['domanda']}\n{item['risposta']}",
+        metadata={"categoria": item["categoria"], "id": item["id"]}
+    )
+    for item in MEDICAL_DOCS
+]
 
 embeddings = HuggingFaceEmbeddings()
 
-db = FAISS.from_documents(documents, embeddings)
+db = FAISS.from_documents(docs, embeddings)
 
 def retrieve_context(query):
     docs = db.similarity_search(query, k=3)
